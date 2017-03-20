c358df03e06ae04c2fdddbb6615fa4350a0ef5c5
DoddapaneniAkhila <doddapaneniakhi14@gmail.com>
2017-03-20 19:57:32 -0400

    Add files via upload

---
+++ LogicCode.java
@@ -0,0 +1,27 @@
+package com.tutorial.java;
+
+public class LogicCode {
+	
+	public  Integer addition(int a, int b){
+
+		  int c = a+b;
+		    System.out.println("Total sum of Two numbers: " +c);
+			return c;
+		
+	}
+	public  void addition(int a, double b){
+	     
+		
+	    double c = a+b;
+	    System.out.println("Total sum of Two numbers: " +c);
+	
+}
+	public  void addition(int a, int b,int d){
+	     
+		
+	    int c = a+b+d;
+	    System.out.println("Total sum of Two numbers: " +c);
+	
+}
+
+}
---
+++ SampleExample.java
@@ -0,0 +1,18 @@
+package com.tutorial.java;
+
+
+public class SampleExample {
+
+	public static void main(String[] args) {
+		
+	  SampleExample a = new SampleExample();
+	   LogicCode b = new LogicCode();
+	   
+	   b.addition(5,6)   ;
+  
+
+	}
+	
+	
+
+}
